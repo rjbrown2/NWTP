5bfbd8b0d682d1658a408ddc718fb08e6d918e05
Robert Brown <Robert.Brown@tmctechnologies.com>
2021-12-24 22:33:14 -0500

    First Push

---
+++ UI.py
@@ -0,0 +1,76 @@
+from PyQt5 import QtWidgets, uic
+import sys
+import math
+
+import constants
+import recipies
+import signal
+
+from PyQt5.QtCore import QCoreApplication
+
+class Ui(QtWidgets.QWidget):
+    def __init__(self, parent=None):
+        super(Ui, self).__init__()
+        #self.ui = loadUI(os.path.join(root, 'NW_TP3.ui'), baseinstance=self)
+        uic.loadUi('NW_TP3.ui', self)
+        self.setWindowTitle("New World Trading Post")
+        self.combo = self.findChild(QtWidgets.QComboBox,"buy_comboBox")
+        self.combo.currentTextChanged.connect(self.buy_combo_selected)
+        self.buyQuantity = self.findChild(QtWidgets.QLineEdit, "buyQuantity")
+        self.buyIndividual = self.findChild(QtWidgets.QLineEdit, "buyIndividual")
+        self.buyFlip = self.findChild(QtWidgets.QLineEdit, "buyFlip")
+        self.capital = self.findChild(QtWidgets.QLineEdit, "Capital")
+        self.debug = self.findChild(QtWidgets.QTextEdit, "debug")
+        self.show()
+
+    def sell_combo_selected(self):
+        pass
+
+    def buy_combo_selected(self):
+        item = self.combo.currentText()
+
+        #TODO:  Add Error Checking for Capital.  MUST ONLY BE POSITIVE Float
+        
+        buy_individual, buy_profit, buy_price, sell_price, captial = 0.0 , 0.0, 0.0, 0.0, 0.0
+        can_buy = 0
+        transpose = ""
+
+        capital = float(self.capital.text())
+
+        for line in line_list:
+            if item == line[0]:
+                buy_price = float(line[1])
+                sell_price = float(line[2])
+                break
+
+        can_buy = math.floor(capital /buy_price)
+        buy_individual = sell_price * can_buy
+        buy_profit =  buy_individual - capital
+        print("You Selected: " + item)
+        for line in dict:
+            if line[1] == item:
+                transpose = line[0]
+                break
+        self.buyQuantity.setText(str(can_buy))
+        self.buyIndividual.setText(str(buy_individual))
+        self.buyFlip.setText(str(buy_profit))
+
+if __name__ == "__main__":
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
+    line_list = []
+    dict = constants.dict
+
+    with open("Ingredients.txt", "r") as read_file:
+        lines = read_file.readlines()
+    for line in lines:
+        as_list = line.split(",")
+        line_list.append(as_list) 
+    app = QtWidgets.QApplication(sys.argv)
+    window = Ui()
+    window.show()
+    app.exec_()
+
+
+
+
+
---
+++ recipies.py
@@ -0,0 +1,88 @@
+import constant
+
+
+class Recipe:
+    def __init__(self, recipe, ingrs, qtys, has_sub_recipe=False, sub_recipe=None):
+        self.recipe = recipe
+        self.ingrs = ingrs
+        self.qtys = qtys
+        self.has_sub_recipe = has_sub_recipe
+        self.sub_recipe = sub_recipe
+
+
+def pull_recipe(recipe_name):
+    ingrlist = []
+    qtylist = []
+    recipe_found = False
+
+    try:
+        file = open(constant.RECIPE_DUMP)
+
+        lines = file.readlines()
+        for line in lines:
+            if line.split(",")[0] == recipe_name:
+                # Recipe Found
+                # Check Ingredients required and add them to lists
+                if bool(line.split(",")[36]):  # Ingredient 1
+                    ingrlist.append(line.split(",")[36])
+                    qtylist.append(line.split(",")[50])
+                if bool(line.split(",")[38]):  # Ingredient 2
+                    ingrlist.append(line.split(",")[38])
+                    qtylist.append(line.split(",")[51])
+                if bool(line.split(",")[40]):  # Ingredient 3
+                    ingrlist.append(line.split(",")[40])
+                    qtylist.append(line.split(",")[52])
+                if bool(line.split(",")[42]):  # Ingredient 4
+                    ingrlist.append(line.split(",")[42])
+                    qtylist.append(line.split(",")[53])
+                if bool(line.split(",")[44]):  # Ingredient 5
+                    ingrlist.append(line.split(",")[44])
+                    qtylist.append(line.split(",")[54])
+                if bool(line.split(",")[46]):  # Ingredient 6
+                    ingrlist.append(line.split(",")[46])
+                    qtylist.append(line.split(",")[55])
+                if bool(line.split(",")[48]):  # Ingredient 7
+                    ingrlist.append(line.split(",")[48])
+                    qtylist.append(line.split(",")[56])
+
+                # recursively check for sub recipes
+                for r in ingrlist:
+                    sub_rec = pull_recipe(r)
+                    if sub_rec is not None:
+                        # Found a sub recipe
+                        has_sub_recipe = True
+                        sub_recipe = sub_rec
+                        return Recipe(recipe_name, ingrlist, qtylist, has_sub_recipe, sub_recipe)
+
+                return Recipe(recipe_name, ingrlist, qtylist)
+
+            if recipe_found:
+                break
+
+        if not recipe_found:
+            return None
+
+        file.close()
+    except FileNotFoundError:
+        print("File not found!")
+
+    input("PRESS")
+
+
+def main():
+    recipe = pull_recipe("IngotT4")
+
+    # This recursion works yo
+    while recipe.has_sub_recipe:
+        print(recipe.recipe + " requires:")
+        for i, j in zip(recipe.ingrs, recipe.qtys):
+            print(i + " " + j)
+        print("\n")
+        recipe = recipe.sub_recipe
+    print(recipe.recipe + " requires:")
+    for i, j in zip(recipe.ingrs, recipe.qtys):
+        print(i + " " + j)
+
+
+if __name__ == "__main__":
+    main()
